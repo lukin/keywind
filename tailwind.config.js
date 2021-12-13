766a38b2f2d186ae2c4c84a35ae76e18731066cf
@lukin <anton@lukin.dev>
2021-12-14 00:50:00 +0700

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,8 @@ const colors = require('tailwindcss/colors');
  * @type { import('@types/tailwindcss/tailwind-config').TailwindConfig }
  */
 module.exports = {
-  darkMode: 'media',
+  content: ['./theme/**/*.ftl'],
   plugins: [require('@tailwindcss/forms')],
-  purge: ['./theme/**/*.ftl'],
   theme: {
     extend: {
       colors: {
