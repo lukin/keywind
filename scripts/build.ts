48ad13b98fd1d79ba8fde6296e82a9c5121776d0
CN <31734342+cn42@users.noreply.github.com>
2024-01-04 09:59:47 +0100

    Update build.ts
    Rename output folder

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,14 +3,13 @@ import { createWriteStream, existsSync, mkdirSync } from 'fs';
 
 import { name } from '../package.json';
 
-const dir = 'out';
+const dir = 'dist';
 const file = `${name}.jar`;
 const path = `${dir}/${file}`;
 
 !existsSync(dir) && mkdirSync(dir);
 
 const output = createWriteStream(`${__dirname}/../${path}`);
-
 const archive = archiver('zip');
 
 archive.on('error', (error) => {
